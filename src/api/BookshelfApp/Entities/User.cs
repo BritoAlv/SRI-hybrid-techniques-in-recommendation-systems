fdc8995bfc41c65efcea463cdc2fb23e8b80749a
Javier Lima <javi.limashelby@gmail.com>
2024-08-22 18:24:31 -0400

    feat: Add Features property to User
    - Migrate changes

--- src/api/BookshelfApp/Entities/User.cs
+++ src/api/BookshelfApp/Entities/User.cs
@@ -9,6 +9,7 @@ public class User
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string Email { get; set; } = null!;
+    public string? Features { get; set; } = null!;
 
     // Relational Properties
     public ICollection<UserBook> UserBooks { get; set; } = null!;
--- src/api/BookshelfApp/Migrations/20240822221239_Add 'features' column in Users.Designer.cs
+++ src/api/BookshelfApp/Migrations/20240822221239_Add 'features' column in Users.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookshelfApp.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookshelfApp.Migrations
 {
     [DbContext(typeof(BookshelfContext))]
-    partial class BookshelfContextModelSnapshot : ModelSnapshot
+    [Migration("20240822221239_Add 'features' column in Users")]
+    partial class AddfeaturescolumninUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
